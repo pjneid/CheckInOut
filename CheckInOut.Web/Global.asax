b3e0643a07a090624ee0ce7a07c363dcebeedcf4
pjneid <pjneid@hotmail.com>
2015-06-16 15:53:39 +1000

    add to github

---
+++ CheckInOut.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CheckInOut.Web.MvcApplication" Language="C#" %>
---
+++ CheckInOut.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CheckInOut.Web.Startup))]
+namespace CheckInOut.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
