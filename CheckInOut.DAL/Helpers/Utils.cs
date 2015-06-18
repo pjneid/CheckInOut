d93a821a193c7a4a1f351676c73f4777acd0f851
pjneid <pjneid@hotmail.com>
2015-06-18 12:07:31 +1000

    CODING

---
+++ CheckInOut.DAL/Helpers/Utils.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using CheckInOut.DAL.Context;
+using CheckInOut.DAL.Models;
+
+namespace CheckInOut.DAL.Helpers
+{
+    public class Utils
+    {
+        private readonly CheckInOutContext _context = new CheckInOutContext();
+
+        public string GetVisitorAccessNumber(int companyId)
+        {
+
+            const string chars = "0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, 4)
+                    .Select(s => s[random.Next(s.Length)])
+                    .ToArray());
+
+            while (_context.Visitors.Any(s => s.AccessNumber == result && s.CompanyId == companyId))
+            {
+                result = new string(
+                    Enumerable.Repeat(chars, 4)
+                        .Select(s => s[random.Next(s.Length)])
+                        .ToArray());
+            }
+            return result;
+        }
+
+        public string GetEmployeeAccessNumber(int branchId)
+        {
+
+            const string chars = "0123456789";
+            var random = new Random();
+            var result = new string(
+                Enumerable.Repeat(chars, 4)
+                    .Select(s => s[random.Next(s.Length)])
+                    .ToArray());
+
+            while (_context.Employees.Any(s => s.AccessNumber == result && s.BranchId == branchId))
+            {
+                result = new string(
+                    Enumerable.Repeat(chars, 4)
+                        .Select(s => s[random.Next(s.Length)])
+                        .ToArray());
+            }
+            return result;
+        }
+
+        public bool SignInMail(string toAddress, string subject, string body, int companyId)
+        {
+            try
+            {
+                var toEmails = toAddress.Split(';');
+
+                var configurationEmail = _context.ConfigurationEmails.FirstOrDefault(s => s.CompanyId == companyId);
+                if (configurationEmail != null)
+                {
+                    var sender = new MailAddress(configurationEmail.UserName, configurationEmail.Company.Name);
+
+                    var smtp = new SmtpClient
+                    {
+                        Host = configurationEmail.SmtpClient,
+                        Port = configurationEmail.Port,
+                        EnableSsl = configurationEmail.EnableSsl,
+                        UseDefaultCredentials = false,
+                        DeliveryMethod = SmtpDeliveryMethod.Network,
+                        Credentials = new NetworkCredential(sender.Address, configurationEmail.Password),
+                        Timeout = 20000
+                    };
+
+                    var msg = new MailMessage
+                    {
+                        From = sender,
+                        Subject = subject,
+                        Body = body + Environment.NewLine + Environment.NewLine +
+                               "*This is an automated email please do not respond*",
+                    };
+
+                    for (int i = 0; i < toEmails.Count(); i++)
+                    {
+                        msg.To.Add(new MailAddress(toEmails[i]));
+                    }
+
+                    var ccEmails = configurationEmail.CcEmails.Split(';');
+                    for (int j = 0; j < ccEmails.Count(); j++)
+                    {
+                        msg.CC.Add(new MailAddress(ccEmails[j]));
+                    }
+
+                    smtp.Send(msg);
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        //public bool SignInMail(string toAddress, string subject, string body, int companyId)
+        //{
+        //    try
+        //    {
+        //        var toEmails = toAddress.Split(';');
+
+        //        var configurationEmail = _context.ConfigurationEmails.FirstOrDefault(s => s.CompanyId == companyId);
+        //        if (configurationEmail != null)
+        //        {
+        //            SmtpClient mySmtpClient = new SmtpClient(configurationEmail.SmtpClient);
+
+        //            // set smtp-client with basicAuthentication
+        //            mySmtpClient.UseDefaultCredentials = false;
+        //            NetworkCredential basicAuthenticationInfo = new
+        //                NetworkCredential(configurationEmail.UserName, configurationEmail.Password);
+        //            mySmtpClient.Credentials = basicAuthenticationInfo;
+
+        //            // add from,to mailaddresses
+        //            MailAddress from = new MailAddress(configurationEmail.UserName, configurationEmail.Company.Name);
+        //            //MailAddress to = new MailAddress("test2@example.com", "TestToName");
+
+
+
+        //            //MailMessage myMail = new MailMessage(from, to);
+
+        //            MailMessage myMail = new MailMessage
+        //            {
+        //                From = from,
+        //                Subject = subject,
+        //                Body = body + Environment.NewLine + Environment.NewLine +
+        //                       "*This is an automated email please do not respond*",
+        //            };
+
+        //            for (int i = 0; i < toEmails.Count(); i++)
+        //            {
+        //                myMail.To.Add(new MailAddress(toEmails[i]));
+        //            }
+
+        //            // add ReplyTo
+        //            //MailAddress replyto = new MailAddress("reply@example.com");
+        //            //myMail.ReplyTo = replyto;
+
+        //            // set subject and encoding
+        //            //myMail.Subject = "Test message";
+        //            myMail.SubjectEncoding = Encoding.UTF8;
+
+        //            // set body-message and encoding
+        //           // myMail.Body = "<b>Test Mail</b><br>using <b>HTML</b>.";
+        //            myMail.BodyEncoding = Encoding.UTF8;
+        //            // text or html
+        //            myMail.IsBodyHtml = true;
+
+        //            mySmtpClient.Send(myMail);
+        //        }
+        //        return true;
+        //    }
+
+        //    catch (SmtpException ex)
+        //    {
+        //        throw new ApplicationException
+        //            ("SmtpException has occured: " + ex.Message);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw ex;
+        //    }
+        //}
+
+        public Branch GetBranch(int deviceId)
+        {
+            return _context.Branches.FirstOrDefault(s => s.Devices.Any(d => d.Id == deviceId));
+        }
+
+    }
+}
--- CheckInOut.Display/Controllers/HomeController.cs
+++ CheckInOut.Display/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
-using System.Web;
+using System.Threading;
 using System.Web.Mvc;
 using CheckInOut.DAL.Context;
+using CheckInOut.DAL.Helpers;
 using CheckInOut.DAL.Mappers;
+using CheckInOut.DAL.Models;
 using CheckInOut.DAL.ViewModels;
 using CheckInOut.Display.Helpers;
 
@@ -21,18 +24,238 @@ namespace CheckInOut.Display.Controllers
             return View(model);
         }
 
-        public ActionResult About()
+        public ActionResult SignIn(int deviceVisitorTypeId = -1)
         {
-            ViewBag.Message = "Your application description page.";
+            if (deviceVisitorTypeId == -1)
+                return RedirectToAction("Index");
 
+            var deviceVisitorTypeDataFields =
+                _context.DeviceVisitorTypeDataFields.Where(s => s.DeviceVisitorTypeId == deviceVisitorTypeId && s.IsActive)
+                    .ToList();
+
+            var model = deviceVisitorTypeDataFields.Select(
+                    e => new VmDeviceVisitorTypeDataField().MapFromDeviceVisitorTypeDataFields(e)).ToList();
+
+            ViewData["VisitorTypeId"] = deviceVisitorTypeId;
+            return View(model);
+        }
+
+        public ActionResult ThankYou(FormCollection frmCollection)
+        {
+            if (frmCollection.Count == 0)
+                return RedirectToAction("Index");
+
+            var vmSignIn = new VmSignIn
+            {
+                Name = frmCollection["txtName"],
+                Company = frmCollection["txtCompany"],
+                Email = frmCollection["txtEmail"],
+                Mobile = frmCollection["txtMobile"],
+                VisitorTypeId = Convert.ToInt32(frmCollection["txtVisitorType"]),
+                DeviceId = SettingsManager.DeviceId,
+                EmployeeId =
+                    (frmCollection["txtSelectEmployeeId"].Equals("-1")
+                        ? (int?)null
+                        : Convert.ToInt32(frmCollection["txtSelectEmployeeId"]))
+            };
+
+            Utils utils = new Utils();
+            string accessNumber;
+            if (vmSignIn.VisitorTypeId.Equals(3))// Employee
+            {
+                int branchId = _context.Devices.FirstOrDefault(s => s.Id == vmSignIn.DeviceId).Branch.Id;
+
+                var employee = new Employee
+                {
+                    Name = vmSignIn.Name,
+                    Email = vmSignIn.Email,
+                    Mobile = vmSignIn.Mobile,
+                    IsActive = true,
+                    CreatedDate = DateTime.Now,
+                    BranchId = branchId,
+                    AccessNumber = utils.GetEmployeeAccessNumber(branchId)
+                };
+
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+
+                SignInTransaction(vmSignIn.DeviceId, null, employee.Id);
+
+                accessNumber = employee.AccessNumber;
+            }
+            else
+            {
+                int companyId = _context.Devices.FirstOrDefault(s => s.Id == vmSignIn.DeviceId).Branch.CompanyId;
+                //Visitor or Contractor
+                var visitor = new Visitor
+                {
+                    Name = vmSignIn.Name,
+                    Company = vmSignIn.Company,
+                    Email = vmSignIn.Email,
+                    Mobile = vmSignIn.Mobile,
+                    AccessNumber = utils.GetVisitorAccessNumber(companyId),
+                    IsActive = true,
+                    CreatedDate = DateTime.Now,
+                    VisitorTypeId = vmSignIn.VisitorTypeId,
+                    CompanyId = companyId
+                };
+
+                _context.Visitors.Add(visitor);
+                _context.SaveChanges();
+
+                SignInTransaction(vmSignIn.DeviceId, visitor.Id, vmSignIn.EmployeeId);
+
+                if (vmSignIn.EmployeeId != null)
+                {
+                    string toEmail = _context.Employees.FirstOrDefault(s => s.Id == vmSignIn.EmployeeId).Email;
+                    //int companyId = _db.Devices.FirstOrDefault(s => s.Id == vmSignIn.DeviceId).Branch.CompanyId;
+                    if (!toEmail.Equals(""))
+                    {
+                        //_utils.SignInMail(toEmail, "Reception Notice",
+                        //visitor.Name + " has arrived at reception", companyId);
+
+                        ThreadStart threadStart = delegate
+                        {
+                            utils.SignInMail(toEmail, "Reception Notice",
+                                visitor.Name + " has arrived at reception", companyId);
+                        };
+                        Thread thread = new Thread(threadStart);
+                        thread.Start();
+                    }
+                }
+               accessNumber= visitor.AccessNumber;
+            }
+
+            ViewData["AccessNumber"] = accessNumber;
             return View();
         }
 
-        public ActionResult Contact()
+        private void SignInTransaction(int deviceInId, int? visitorId, int? employeeId)
         {
-            ViewBag.Message = "Your contact page.";
+            var transaction = new TrxVisit
+            {
+                SignedIn = true,
+                SignedOut = null,
+                TimeIn = DateTime.Now,
+                TimeOut = null,
+                IsActive = true,
+                CreatedDate = DateTime.Now,
+                DeviceInId = deviceInId,
+                DeviceOutId = null,
+                VisitorId = visitorId,
+                EmployeeId = employeeId
+            };
 
+            _context.TrxVisits.Add(transaction);
+            _context.SaveChanges();
+
+        }
+
+        public ActionResult SignOut()
+        {
             return View();
+        }
+
+        public ActionResult GetSignedIn()
+        {
+            var deviceIds =
+                    _context.Devices.FirstOrDefault(s => s.Id == SettingsManager.DeviceId).Branch.Devices.Select(s => s.Id).ToArray();
+
+            var vignedInVisits =
+                _context.TrxVisits.Include("Visitor").Include("Employee")
+                    .Where(s => s.SignedOut == null && deviceIds.Contains(s.DeviceInId))
+                    .ToList();
+
+            var personsViewModel = vignedInVisits.Select(e => new VmSignOut().MapFromTrxVisit(e)).ToList();
+
+
+            return Json(personsViewModel);
+        }
+
+        public ActionResult GetEmployees()
+        {
+            int branchId = _context.Devices.FirstOrDefault(s => s.Id == SettingsManager.DeviceId).Branch.Id;
+
+            var employees =
+                _context.Employees.Where(s => s.BranchId == branchId && s.IsActive).OrderBy(s => s.Name).ToList();
+            var lstEmployees = employees.Select(e => new VmEmployee().MapFromEmployee(e)).ToList();
+
+
+            return Json(lstEmployees);
+        }
+
+        public ActionResult GetEmployeeByCode(string code)
+        {
+            var isEmployee =
+                     _context.Employees.FirstOrDefault(s => s.AccessNumber == code && s.IsActive);
+
+            VmEmployee employee = null;
+
+            if (isEmployee != null)
+                employee= new VmEmployee()
+                {
+                    Id = isEmployee.Id,
+                    Name = isEmployee.Name,
+                    Email = isEmployee.Email,
+                    Mobile = isEmployee.Mobile,
+                    AccessNumber = isEmployee.AccessNumber
+                };
+
+
+            return Json(employee);
+        }
+
+        public ActionResult GetVisitorByCode(string code)
+        {
+            var isVisitor =
+                    _context.Visitors.FirstOrDefault(s => s.AccessNumber == code && s.IsActive);
+
+            VmVisitor visitor = null;
+            if (isVisitor != null)
+                visitor= new VmVisitor()
+                {
+                    Id = isVisitor.Id,
+                    Name = isVisitor.Name,
+                    Email = isVisitor.Email,
+                    Mobile = isVisitor.Mobile,
+                    AccessNumber = isVisitor.AccessNumber,
+                    Company = isVisitor.Company,
+                    VisitorType = isVisitor.VisitorType.Name
+                };
+
+            return Json(visitor);
+        }
+
+        public ActionResult SignOutVisitor(int visitId)
+        {
+            TrxVisit trxVisit = _context.TrxVisits.Find(visitId);
+
+            trxVisit.SignedOut = true;
+            trxVisit.TimeOut = DateTime.Now;
+            trxVisit.DeviceOutId = SettingsManager.DeviceId;
+
+            _context.Entry(trxVisit).State = EntityState.Modified;
+
+            bool isDone;
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TrxVisitExists(visitId))
+                {
+                    isDone= false;
+                }
+                throw;
+            }
+
+            return Json("done");
+        }
+
+        private bool TrxVisitExists(int id)
+        {
+            return _context.TrxVisits.Count(e => e.Id == id) > 0;
         }
     }
 }
